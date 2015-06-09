5dfc249df2152d0ba807140833b231d6eab8fdc3
AlexanderMichael <mari12c5898@gmail.com>
2015-06-09 18:32:06 -0500

    Proyecto Animales
    v 1.0

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+$(document).ready(function() {
+	var perro = document.getElementById("perro");
+	var vaca = document.getElementById("vaca");
+	var gato = document.getElementById("gato");
+	var conejo = document.getElementById("conejo");
+	var caballo = document.getElementById("caballo");
+	var elefante = document.getElementById("elefante");
+	var leon = document.getElementById("leon");
+	var osopanda = document.getElementById("osopanda");
+	var oveja = document.getElementById("oveja");
+	var pingüino = document.getElementById("pingüino");
+	var tigre = document.getElementById("tigre");
+});
